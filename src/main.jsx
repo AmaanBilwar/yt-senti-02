6dfb6705e7cdd12d3df73c11abd666844ffbd23d
Amaan <121273095+AmaanBilwar@users.noreply.github.com>
2024-06-25 13:59:46 -0400

    feat: routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  {
+    path:"/yt-senti-02/",
+    element: <App />,
+    children: [
+      {
+        path:'/',
+        element: <Home />
+      },
+      {
+        path:'/home',
+        element: <Home />
+      },
+      {
+        path:'/about',
+        element: <About />
+      },
+    ]
+  }
+])
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
